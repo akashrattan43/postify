247d92549944f6222029b506cfed38ae9d0a7c1a
akash rattan <akashrattan43@Gmail.com>
2021-02-13 14:09:05 -0800

    finished

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
-const {ApolloServer} = require('apollo-server')
-const gql = require('graphql-tag')
-const mongoose = require('mongoose')
-const {MONGODB} = require('./config.js')
 
+const { ApolloServer, PubSub } = require('apollo-server');
+const mongoose = require('mongoose');
 
-const typeDefs = gql`
-    type Query {
-       sayHi: String!
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
+const { MONGODB } = require('./config.js');
 
-    }
-`
+const pubsub = new PubSub();
 
-const resolvers = {
-    Query: {
-        sayHi: () => 'Hello World!!!'
-     }
-}
+const PORT = process.env.port || 5000;
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers
-})
+  typeDefs,
+  resolvers,
+  context: ({ req }) => ({ req, pubsub })
+});
 
-mongoose.connect(MONGODB, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => {
-        console.log('MongoDB Connected')
-        return server.listen({port:5000})
-    })
-    .then(res => {
-        console.log(`Server running at ${res.url}`)
-    })
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true })
+  .then(() => {
+    console.log('MongoDB Connected');
+    return server.listen({ port: PORT });
+  })
+  .then((res) => {
+    console.log(`Server running at ${res.url}`);
+  })
+  .catch(err => {
+    console.error(err)
+  })
